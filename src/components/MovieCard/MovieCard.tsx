7bb0ae805f9e54f0fb903a46a6142058172e9a6b
Francisco Grande <fjgrandv@gmail.com>
2023-11-08 00:20:28 +0100

    Add type picture interface

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -5,13 +5,20 @@ interface MovieCardProps {
 }
 
 const MovieCard = ({
-  movie: { name, director, year },
+  movie: { name, director, year, picture },
 }: MovieCardProps): React.ReactElement => {
   return (
     <div className="card">
-      <h2 className="card__title">movie: {name}</h2>
-      <span className="card__year">release: {year}</span>
-      <span className="card__director">director: {director} </span>
+      <h2 className="card__title">{name}</h2>
+      <img
+        className="card__picture"
+        src={picture}
+        alt={name}
+        width="225"
+        height="300"
+      />
+      <span className="card__year">Release: {year}</span>
+      <span className="card__director">Director: {director} </span>
     </div>
   );
 };
