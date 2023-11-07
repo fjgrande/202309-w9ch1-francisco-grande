7552879ea867ac94dbe21da05e131134da534ebc
Francisco Grande <fjgrandv@gmail.com>
2023-11-07 19:48:23 +0100

    Add HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = (): React.ReactElement => {
+  return (
+    <section className="main-container">
+      <h2 className="page-title">Ganster Movies List</h2>
+    </section>
+  );
+};
+
+export default HomePage;
