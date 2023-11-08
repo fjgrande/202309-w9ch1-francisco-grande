bd760902ee4e3aef0ceda9a8465c5b0f4b568abe
Francisco Grande <fjgrandv@gmail.com>
2023-11-08 23:36:54 +0100

    Add checbox and dispatch action toggleWatchMovies

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,13 +1,22 @@
+import { useDispatch } from "react-redux";
 import { MovieStructure } from "../../store/feature/movies/types";
 import MovieCardStyled from "./MovieCardStyled";
+import { useCallback } from "react";
+import { toggleWatchMoviesActionCreator } from "../../store/feature/movies/moviesSlice";
 
 interface MovieCardProps {
   movie: MovieStructure;
 }
 
 const MovieCard = ({
-  movie: { name, director, year, picture },
+  movie: { name, director, year, picture, isSeen, id },
 }: MovieCardProps): React.ReactElement => {
+  const dispatch = useDispatch();
+
+  const onchange = useCallback(() => {
+    dispatch(toggleWatchMoviesActionCreator(id));
+  }, [dispatch, id]);
+
   return (
     <MovieCardStyled>
       <h2 className="card__title">{name}</h2>
@@ -20,6 +29,15 @@ const MovieCard = ({
       />
       <span className="card__year">Release: {year}</span>
       <span className="card__director">Director: {director} </span>
+      <label>
+        Movie is seen?
+        <input
+          type="checkbox"
+          className="card__checkbox"
+          checked={isSeen}
+          onChange={onchange}
+        />
+      </label>
     </MovieCardStyled>
   );
 };
