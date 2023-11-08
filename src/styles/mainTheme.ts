d539d54f718967e0950b0846808714159e3965a5
Francisco Grande <fjgrandv@gmail.com>
2023-11-08 19:28:54 +0100

    Add mainTheme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    main: "#ffffff",
+    accent: "#000000",
+    light: "#46462f",
+  },
+  typography: {
+    mainFont:
+      "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;",
+  },
+};
+
+export default mainTheme;
